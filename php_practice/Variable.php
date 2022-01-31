6b5d3117a82a11d8aaf341d286da61d68b8cdeca
Harsh Jilka <iharshjilka.34@gmail.com>
2022-01-31 11:29:00 +0530

    Files Added

---
+++ php_practice/Variable.php
@@ -0,0 +1,36 @@
+<?php 
+	
+	echo "<pre>";
+
+	$text ="Hello world!"; // Declaration STRING
+	echo("<br>");
+
+	$x=15; // Declaration INT
+	$y=20;
+	$z=$x+$y;
+
+	
+	echo $text;
+	echo"<br>";
+
+	echo($z); // print variable by echo 
+	echo("<br>");
+
+	var_dump($z); // print variable by var_dumb
+	echo("<br>");
+
+	print_r($z); // print varible by print_r
+	echo("<br>");
+	
+	// Concatination
+	$name1 = "Harsh";
+	$name2 = "Jilka";
+	echo $name1. " " .$name2;
+	echo("<br>");
+
+	// Array 
+	$company = array("Cybercom","creation","PHP_DEVLOPER"); 
+	echo count($company);
+
+	 		
+?>
